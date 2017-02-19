218c6fad32f8ef924d60cb54a015db0276199583
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-02-19 17:55:07 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get "/" do 
+		"Hello, World!"
+	end
+
+	get "/name" do 
+		"My name is Frederico"
+	end
+
+	get "/hometown" do 
+		"My hometown is Brooklyn"
+	end
+
+	get "/favorite-song" do 
+		"My favorite song is the Nyan Cat theme song"
+	end
+
 end
